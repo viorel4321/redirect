f34b290ced62e97ecf0d8ca818877bee71ce91c7
viorel4321 <66820333+viorel4321@users.noreply.github.com>
2024-05-30 17:55:08 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define ('HOST', '109.94.209.115');
-define ('USER', 'la2interlude');
-define ('PASSWORD', 'leroiv461');
-define ('DB_NAME', 'la2interlude');
+define ('USER', '****');
+define ('PASSWORD', '****');
+define ('DB_NAME', '****');
 ?>
